304ec0f654b84a778c3829f78ffd0a473f715f64
Mike <menzelmike123@gmail.com>
2021-12-12 17:32:11 +0100

    add run methods
    The run methods load the next image for creating the run effect.
    The dino can run upright (with and without bazooka)
    and stooped (only without bazooka).

--- Dino.py
+++ Dino.py
@@ -12,18 +12,35 @@ class Dino:
     __game_over_sprite_path = os.path.join("sprites", "dino", "trex04.png")
     __game_over_bazooka_sprite_path = os.path.join("sprites", "dino", "trexBazooka04.png")
 
+    __jump_height = 10
+
     def __init__(self, x, y):
         self.__x = x
         self.__y = y
         self.__dino_image = pygame.image.load(Dino.__running_sprite_paths[0])
         self.__has_bazooka = False
         self.__running_counter = 0
+        self.__running_counter = 0
+        self.__jump_counter = 0
 
     def run(self):
-        pass
+        if self.__has_bazooka:
+            if self.__running_counter >= len(Dino.__bazooka_sprite_paths):
+                self.__running_counter = 0
+            self.__dino_image = pygame.image.load(Dino.__bazooka_sprite_paths[self.__running_counter])
+        else:
+            if self.__running_counter >= len(Dino.__running_sprite_paths):
+                self.__running_counter = 0
+            self.__dino_image = pygame.image.load(Dino.__running_sprite_paths[self.__running_counter])
+
+        self.__running_counter += 1
 
     def run_stooping(self):
-        pass
+        if self.__running_counter >= len(Dino.__stooping_sprite_paths):
+            self.__running_counter = 0
+        self.__dino_image = pygame.image.load(Dino.__stooping_sprite_paths[self.__running_counter])
+
+        self.__running_counter += 1
 
     def jump(self):
         pass
