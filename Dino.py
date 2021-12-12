26e5e20c2662398ce23dd46edbf38aa24a34b531
Mike <menzelmike123@gmail.com>
2021-12-12 16:55:33 +0100

    add basic structure of dino class

---
+++ Dino.py
@@ -0,0 +1,41 @@
+import os.path
+
+import pygame.image
+import glob
+
+
+class Dino:
+    __running_sprite_paths = [f for f in glob.glob(os.path.join("sprites", "dino", "dino_running_normal", "*.png"))]
+    __stooping_sprite_paths = [f for f in glob.glob(os.path.join("sprites", "dino", "dino_running_stooping", "*.png"))]
+    __bazooka_sprite_paths = [f for f in glob.glob(os.path.join("sprites", "dino", "dino_running_bazooka", "*.png"))]
+    __jumping_sprite_path = os.path.join("sprites", "dino", "dino_running_normal", "trex04.png")
+    __game_over_sprite_path = os.path.join("sprites", "dino", "trex04.png")
+    __game_over_bazooka_sprite_path = os.path.join("sprites", "dino", "trexBazooka04.png")
+
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
+        self.__dino_image = pygame.image.load(Dino.__running_sprite_paths[0])
+        self.__has_bazooka = False
+        self.__running_counter = 0
+
+    def run(self):
+        if self.__has_bazooka:
+            if self.__running_counter >= len(Dino.__bazooka_sprite_paths):
+                self.__running_counter = 0
+            self.__dino_image = pygame.image.load(Dino.__bazooka_sprite_paths[self.__running_counter])
+        else:
+            if self.__running_counter >= len(Dino.__running_sprite_paths):
+                self.__running_counter = 0
+            self.__dino_image = pygame.image.load(Dino.__running_sprite_paths[self.__running_counter])
+
+        self.__running_counter += 1
+
+    def run_stooping(self):
+        pass
+
+    def jump(self):
+        pass
+
+    def getImage(self):
+        return self.__dino_image
--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 import pygame as pygame
 
+from Dino import Dino
+
+DISPLAY_WIDTH = 500
+DISPLAY_HEIGHT = 500
+WHITE = (255, 255, 255)
+FPS = 30
+clock = pygame.time.Clock()
+
+quit = False
+dino = Dino(30, 30)
+
 pygame.init()
+gameDisplay = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+
+while not quit:
+    gameDisplay.fill(WHITE)
+    gameDisplay.blit(dino.getImage(), (50, 50))
+    dino.run()
+
+    pygame.display.update()
+    clock.tick(FPS)
