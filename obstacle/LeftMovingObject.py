2ba6f3d1356a128c0f759d79f62e98b105ca1173
Mike <menzelmike123@gmail.com>
2021-12-15 21:11:05 +0100

    add abstract class LeftMovingObject
    this class describes the basic structure of every game object
    which is moving left

---
+++ obstacle/LeftMovingObject.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+
+
+class LeftMovingObject(ABC):
+    def __init__(self, x, y, image, speed=10):
+        self._x = x
+        self._y = y
+        self._image = image
+        self._speed = speed
+
+    def increase_speed(self, increase):
+        self._speed += increase
+
+    @abstractmethod
+    def moveLeft(self):
+        pass
+
+    @abstractmethod
+    def get_collision_box(self):
+        pass
+
+    def get_image(self):
+        return self._image
+
+    def get_x(self):
+        return self._x
+
+    def get_y(self):
+        return self._y
