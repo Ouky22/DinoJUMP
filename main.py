d9682a2e8e95ac76cb907f16ad6641eec29152d0
Mike <menzelmike123@gmail.com>
2021-12-16 23:17:35 +0100

    implement get_collision_boxes()/change move_left()

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from Dino import Dino
 from coin.BazookaCoin import BazookaCoin
 from obstacle.Bird import Bird
 from obstacle.Cactus import Cactus
+from obstacle.Explosion import Explosion
 
 DISPLAY_WIDTH = 1200
 DISPLAY_HEIGHT = 500
@@ -16,16 +17,18 @@ dino = Dino(30, 30)
 cactus = Cactus(700, 400)
 bird = Bird(1100, 400)
 bazookaCoin = BazookaCoin(1100, 300)
+explosion = Explosion(800, 300)
 
 pygame.init()
 gameDisplay = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 
 while not quit:
     gameDisplay.fill(WHITE)
-    gameDisplay.blit(bazookaCoin.get_image(), (bazookaCoin.get_x(), bazookaCoin.get_y()))
-    for box in bazookaCoin.get_collision_boxes():
+    if not explosion.isExplosionOver():
+        gameDisplay.blit(explosion.get_image(), (explosion.get_x(), explosion.get_y()))
+        explosion.moveLeft()
+    for box in explosion.get_collision_boxes():
         pygame.draw.rect(gameDisplay, (255, 0, 0), box, 2)
-    bazookaCoin.moveLeft()
 
     pygame.display.update()
     clock.tick(FPS)
--- obstacle/Explosion.py
+++ obstacle/Explosion.py
@@ -10,7 +10,7 @@ class Explosion(LeftMovingObject):
     __explosion_image_paths = [f for f in glob.glob(os.path.join("sprites", "obstacle", "explosion", "*.png"))]
     # how many times moveLeft() has to be called until
     # the next explosion image is loaded
-    __explosion_speed = 1
+    __explosion_speed = 2
 
     def __init__(self, x, y):
         image = pygame.image.load(Explosion.__explosion_image_paths[0])
@@ -23,17 +23,21 @@ class Explosion(LeftMovingObject):
     def moveLeft(self):
         if self.__running_counter % Explosion.__explosion_speed == 0:
             self.__current_image_index += 1
-            if self.__current_image_index >= len(Explosion.__explosion_image_paths):
-                self.__current_image_index = 0
-                self.__running_counter = 0
+            if self.__current_image_index < len(Explosion.__explosion_image_paths):
+                self._image = pygame.image.load(Explosion.__explosion_image_paths[self.__current_image_index])
+                self._x -= self._speed
+            else:
                 self.__explosion_over = True
-            self._image = pygame.image.load(Explosion.__explosion_image_paths[self.__current_image_index])
 
-        self.__running_counter += 1
-        self._x -= self._speed
+        if not self.__explosion_over:
+            self.__running_counter += 1
 
     def isExplosionOver(self):
         return self.__explosion_over
 
     def get_collision_boxes(self):
-        pass
+        # explosion is not deadly at the end, so return no collision boxes
+        if self.__current_image_index < len(Explosion.__explosion_image_paths) - 6:
+            return [(self._x + self._image.get_width() * 0.3, self._y + self._image.get_height() * 0.4,
+                     self._image.get_width() * 0.5, self._image.get_height() * 0.6)]
+        return []
