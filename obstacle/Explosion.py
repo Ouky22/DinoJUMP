98a698699620aa2b6b3dfaee0a8e2e5c2aa38511
Mike <menzelmike123@gmail.com>
2021-12-16 22:54:43 +0100

    add implementation for moving_left()

--- obstacle/Explosion.py
+++ obstacle/Explosion.py
@@ -8,13 +8,32 @@ from obstacle.LeftMovingObject import LeftMovingObject
 
 class Explosion(LeftMovingObject):
     __explosion_image_paths = [f for f in glob.glob(os.path.join("sprites", "obstacle", "explosion", "*.png"))]
+    # how many times moveLeft() has to be called until
+    # the next explosion image is loaded
+    __explosion_speed = 1
 
     def __init__(self, x, y):
         image = pygame.image.load(Explosion.__explosion_image_paths[0])
         super().__init__(x, y, image)
 
+        self.__running_counter = 0
+        self.__current_image_index = 0
+        self.__explosion_over = False
+
     def moveLeft(self):
-        pass
+        if self.__running_counter % Explosion.__explosion_speed == 0:
+            self.__current_image_index += 1
+            if self.__current_image_index >= len(Explosion.__explosion_image_paths):
+                self.__current_image_index = 0
+                self.__running_counter = 0
+                self.__explosion_over = True
+            self._image = pygame.image.load(Explosion.__explosion_image_paths[self.__current_image_index])
+
+        self.__running_counter += 1
+        self._x -= self._speed
+
+    def isExplosionOver(self):
+        return self.__explosion_over
 
     def get_collision_boxes(self):
         pass
